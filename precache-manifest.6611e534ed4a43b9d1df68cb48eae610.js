9a5ffb167a0de4a1160b16ad24adf100e064902e
william <billy.arrasmith@gmail.com>
2021-01-18 03:24:47 -0600

    Updates

---
+++ precache-manifest.6611e534ed4a43b9d1df68cb48eae610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143c4d35cad41e92c18a82398f3ca35e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33c101c53b238430dc09",
+    "url": "/static/css/main.4604f19f.chunk.css"
+  },
+  {
+    "revision": "a9a1697933440cf343c6",
+    "url": "/static/js/2.7fd7bc47.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7fd7bc47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c101c53b238430dc09",
+    "url": "/static/js/main.10f8b76b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
